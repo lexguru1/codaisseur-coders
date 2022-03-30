c1b74c56083cc1c448b13dc3f90f25e797642aa6
lexguru1 <alexghiurutan@gmail.com>
2022-03-30 13:18:13 +0200

    data is fetched to local state

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function HomePage() {
-  return <h1>Hello!</h1>;
+  const [data, setData] = useState({ loading: true, posts: [] });
+  const url =
+    "https://codaisseur-coders-network.herokuapp.com/posts?offset=2&limit=5";
+  const fetchPosts = async () => {
+    setData({ ...data, loading: true });
+    const response = await axios.get(url);
+    const posts = response.data.rows;
+    setData({ loading: false, posts: posts });
+  };
+  console.log(data.posts);
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      {data.loading
+        ? "Loading"
+        : data.posts.map((post) => {
+            return <p>{post.title}</p>;
+          })}
+    </div>
+  );
 }
